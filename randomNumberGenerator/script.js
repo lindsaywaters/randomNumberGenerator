63463ce2829872f6c6162ddca0e6f14018105911
lindsaywaters <59128405+lindsaywaters@users.noreply.github.com>
2023-06-06 20:34:52 -0400

    randomNumberGenerator

---
+++ randomNumberGenerator/script.js
@@ -0,0 +1,14 @@
+
+const number = document.querySelector(".number");
+const btn = document.querySelector(".generate");
+
+
+const generateNumber = () => {
+    const randomNumber = Math.floor(Math.random() * 10 + 1);
+    number.innerHTML = randomNumber;
+}
+
+btn.addEventListener("click", generateNumber);
+
+// const rand = Math.floor(Math.random() * 10 + 1);
+// console.log(rand);
